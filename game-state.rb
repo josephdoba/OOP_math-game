a21e656a377aa536ce59d63ebc800d016a24ec10
Joseph Doba <josephdoba@gmail.com>
2022-09-05 19:58:00 -0700

    implement while loop and helper functions for game loop logic

--- game-state.rb
+++ game-state.rb
@@ -1,22 +1,19 @@
 # puts "Hello from game-state"
 require "./player.rb"
 
-puts "--- outside the class --- "
-
-puts "--- inside the class --- "
+# puts "--- inside the class --- "
 class GameState
   # Player setup:
-  @jacob = Player.new("Jacob")
-  @coleen = Player.new("Coleen")
-  @active_player = @jacob
-  @passive_player = @coleen
+  @@jacob = Player.new("Jacob")
+  @@coleen = Player.new("Coleen")
+  @@active_player = @@jacob
+  @@passive_player = @@coleen
 
-  puts "the active player is Player #{@active_player.player_id} (#{@active_player.name})"
-  puts "the passive player is Player #{@passive_player.player_id} (#{@passive_player.name})"
-
-  # @jacob.deduct_point
-  # puts @jacob.player_info
-
+  # puts "the active player is Player #{@@jacob.player_id} (#{@@jacob.name})"
+  # puts "the active player is Player #{@@coleen.player_id} (#{@@coleen.name})"
+  # puts "the passive player is Player #{@@active_player.player_id} (#{@@active_player.name})"
+  # puts "the passive player is Player #{@@passive_player.player_id} (#{@@passive_player.name})"
+  
   def initialize
     @@active_game = true
   end
@@ -39,40 +36,51 @@ class GameState
     correct_answer = n1 + n2
     
     puts "--- NEW TURN --- "
-    puts "#{@active_player.name} (Player #{@active_player.player_id}): What is #{n1} + #{n2}?"
+    # puts "#{@@active_player.name} (Player #{@@active_player.player_id}): What is #{n1} + #{n2}?"
+    puts "#{@@active_player.name} (Player #{@@active_player.player_id}): What is #{n1} + #{n2}?"
     @player_answer = Integer(gets.chomp)
 
     if @player_answer == correct_answer
       puts "You are correct!"
     elsif @player_answer != correct_answer
       puts "Wrong! lose a point"
-      @active_player.deduct_point
+      @@active_player.deduct_point
     end
   end
 
   def next_turn(active, next_player)
-    @passive_player = active
-    @active_player = next_player
-    @active_player
+    @@passive_player = active
+    @@active_player = next_player
+    @@active_player
   end
   
   def game_end
-    if @active_player.points == 0 then puts "Player #{player_id} wins with #{} point(s) remaining"
+    if @@active_game == false
+      puts "Player #{@@passive_player.player_id} wins with #{@@passive_player.player_id} point(s) remaining"
     end
   end
 
   def game_status
-    puts "#{@jacob.name} P1: #{@jacob.points}/3 vs #{@coleen.name} P2: #{@coleen.points}/3"
+    puts "#{@@active_player.name} P1: #{@@active_player.points}/3 vs #{@@passive_player.name} P2: #{@@passive_player.points}/3"
   end
 
-    
+  def check_win_condition
+    if @@active_player.points == 0 && @@active_game == true then @@active_game == false
+  end
+
+
   def start
     puts "the game loop has started"
-    seed_question()
-    game_status()
-    next_turn(@active_player, @passive_player)
+    while @@active_game = true do
+      seed_question()
+      check_win_condition()
+      game_status()
+      next_turn(@@active_player, @@passive_player)
+    end
+    game_end()
   end
 
       
     # we need a loop to continually check if the win condition has been met.
+end
 end
